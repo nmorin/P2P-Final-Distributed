0a179c13fc440caaaccf6b1cb65d97381206d638
Megan Maher <mmaher@bowdoin.edu>
2015-04-28 17:10:25 -0400

    Fixes tracker file vs. file

--- TrackerFile.java
+++ TrackerFile.java
@@ -1,3 +1,18 @@
+/* 
+ * Class TrackerFile
+ * Megan Maher and Nicole Morin
+ * Bowdoin College Class of 2016
+ * Distributed Systems
+ *
+ * Created: April 27, 2015
+ * Last Modified: April 27, 2015
+ *
+ * Holds a list with the same number of elements as the number of pieces
+ * of that file. AKA the number of pieces we break the file up into.
+ * Then, for each of these elements, we have a list of userIDs that have
+ * that piece of the file.
+ */
+
 import java.rmi.registry.LocateRegistry;
 import java.rmi.registry.Registry;
 import java.io.*;
@@ -12,18 +27,19 @@ public class TrackerFile {
 
 	public Tracker() {
         super();
+    }
+
+	public int getNumPieces() {
+		return numPieces;
 	}
 
-	public static String query(String fileName) {
-
+	public int getFileSize() {
+		return fileSize;
 	}
 
-	public static void main(String[] argv) {
-
-
-
+	public void updateWithUser(String userID) {
+		ids.add(userID);
 	}
 
-	
-	
 }
+
