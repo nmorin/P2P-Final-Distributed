fab54c5dbbe12fe5059f119feec6739725fc3076
Megan Maher <mmaher@bowdoin.edu>
2015-05-16 11:54:10 -0400

    Adds scp script and changes tracker port

---
+++ scpScript.sh
@@ -0,0 +1,51 @@
+#!/bin/bash
+
+echo $"Virginia, 3"
+scp -i nmorin-keypair -r code nmorin@52.7.147.25:
+
+echo $"California, 3"
+scp -i nmorin-keypair -r code nmorin@52.8.13.61:
+
+echo $"Oregon, 3"
+scp -i nmorin-keypair -r code nmorin@52.11.55.117:
+
+echo $"Ireland, 3"
+scp -i nmorin-keypair -r code nmorin@52.17.118.14:
+
+echo $"Sao Paulo, 3"
+scp -i nmorin-keypair -r code nmorin@54.94.237.85:
+
+echo $"Tokyo, 3"
+scp -i nmorin-keypair -r code nmorin@52.68.70.1:
+
+echo $"Sydney, 3"
+scp -i nmorin-keypair -r code nmorin@52.64.32.152:
+
+echo $"Singapore, 3"
+scp -i nmorin-keypair -r code nmorin@52.74.137.220:
+
+echo "-----------------------------------"
+echo $"Virginia, 4"
+scp -i nmorin-keypair -r code nmorin@52.5.12.102:
+
+echo $"California, 4"
+scp -i nmorin-keypair -r code nmorin@52.8.60.130:
+
+echo $"Oregon, 4"
+scp -i nmorin-keypair -r code nmorin@52.11.50.220:
+
+echo $"Ireland, 4"
+scp -i nmorin-keypair -r code nmorin@52.17.33.248:
+
+echo $"Sao Paulo, 4"
+scp -i nmorin-keypair -r code nmorin@54.94.245.199:
+
+echo $"Tokyo, 4"
+scp -i nmorin-keypair -r code nmorin@52.68.45.217:
+
+echo $"Sydney, 4"
+scp -i nmorin-keypair -r code nmorin@52.64.47.128:
+
+echo $"Singapore, 4"
+scp -i nmorin-keypair -r code nmorin@52.74.155.86:
+
